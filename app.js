b7548a4c24765f8b0e2c213f2636b22380a39e2d
ahmed khaled <ahmed@mrkhalednasr.com>
2021-09-06 11:45:45 +0200

    update app

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const dbURI = "mongodb+srv://FullStackT1:q5ZPvWyCsfWVXtJ@task1.xdlzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+  .then(result => app.listen(process.env.PORT||3000))
   .catch(err => console.log(err));
 
 // register view engine
